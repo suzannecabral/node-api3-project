5fa798624daf74ac6c01fb6088eb188203accd73
Suzanne Cabral <suzannecabral@live.com>
2020-10-29 13:35:01 -0700

    Added dotenv dependency and variable port

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const server = require('./server.js');
 
+
 //listen
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT,()=>{
     console.log(`SERVER IS LISTENING: ${PORT}:`);
 });
