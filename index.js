470aa741803c541d3e7ab0b042b95c9aa27441d6
Suzanne Cabral <suzannecabral@live.com>
2020-10-28 19:08:20 -0700

    Logger middleware works

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code away!
+const server = require('./server.js');
+
+//listen
+const PORT = 8000;
+server.listen(PORT,()=>{
+    console.log(`SERVER IS LISTENING: ${PORT}:`);
+});
--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express');
 
 const server = express();
 
+server.use(logger);
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
- 
+
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  //logs every api request:
+  // - timestamp
+  // - req method
+  // - req url
+  const ts = new Date();
+  console.log(`[${ts.toLocaleTimeString()}] ${req.method} ${req.url}`);
+  next();
+}
 
 module.exports = server;
