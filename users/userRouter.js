a4bd6a099460dd6f189b4416997c4db21940995a
Suzanne Cabral <suzannecabral@live.com>
2020-10-29 20:57:33 -0700

    Added validation for duplicate user, POST user works correctly

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,24 +1,41 @@
 const express = require('express');
 
 //require functions from the db models:
-
 // const { getById } = require('../posts/postDb');
 const Users = require('./userDb');
-
 
 //instantiate router:
 const router = express.Router();
 
-router.post('/', (req, res) => {
+//---------------------------
+// ENDPOINTS
+//---------------------------
+// [ ]
+
+router.post('/', validateUser, duplicateUser, (req, res) => {
   //POST /api/users/
+  // [x]
+  const newUser = req.body;
+  
+  Users.insert(newUser)
+    .then(data=>{
+      console.log(data);
+      res.status(201).json("Created successfully");
+    })
+    .catch(err=>{
+      console.log(err);
+      res.status(500).json({message:'Server error creating new user'});
+    });
 });
 
 router.post('/:id/posts', (req, res) => {
   // POST /api/users/:id/posts
+  // [ ]
 });
 
 router.get('/', (req, res) => {
   // GET /api/users/
+  // [x]
   Users.get()
     .then(data=>{
       res.status(200).json({data});
@@ -82,9 +99,42 @@ function validateUserId(req, res, next) {
 }
 
 //validateUser
-// [ ]
+// [x]
 function validateUser(req, res, next) {
-  // do your magic!
+  //check that there is a response body
+  if(Object.keys(req.body).length > 0){  
+    const {name} = req.body;
+    //check that there is a name
+    if(name){
+      next();
+      //next check that name does not exist
+    }else{
+      res.status(200).json({message:"missing required name field"});
+    }
+  }else{
+    res.status(400).json({message:"missing user data"});
+  }
+}
+
+
+//extra: duplicateUser
+//[x]
+
+function duplicateUser(req, res, next){
+  const {name}=req.body;
+  //check that name does not exist
+  Users.get()
+    .then(data=>{
+      if(data.find(user => user.name === name)){
+        res.status(400).json({message:'Error: User already exists, please pick a unique username.'});
+      }else{
+        next();
+      }
+    })
+    .catch(err=>{
+      console.log(err);
+      res.status(500).json({message:'server error validating user'});
+    })
 }
 
 //validatePost
